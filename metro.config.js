1c3834d8aadc3b5a7c950d56bad26d4b32bf84f8
Chennara Nhes <chennara.nhes@gmail.com>
2026-08-30 16:07:27 +0200

    perf: defer Metro module requires with inlineRequires

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,14 @@ const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
 const config = getSentryExpoConfig(__dirname);
 
+// Defers require() evaluation to first use instead of at bundle load time
+config.transformer.getTransformOptions = async () => {
+  return {
+    transform: {
+      experimentalImportSupport: true,
+      inlineRequires: true,
+    },
+  };
+};
+
 module.exports = config;
